b79b09442d2b7d23ed89e60ab733f064a89917c3
Documenter.jl <documenter@juliadocs.github.io>
2023-11-20 17:52:27 +0000

    build based on 67af03e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = EmpiricalPotentials","category":"page"},{"location":"#EmpiricalPotentials","page":"Home","title":"EmpiricalPotentials","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for EmpiricalPotentials.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [EmpiricalPotentials]","category":"page"}]
+}
