89c4d9308e70610d91687e1dcf3292e989cde2aa
Teemu Järvinen <teemu.j.jarvinen@gmail.com>
2023-12-13 20:50:51 -0800

    add benchmark for parameter estimation

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -13,11 +13,13 @@ SUITE = BenchmarkGroup()
 ## Test PairPotential
 
 SUITE["SimplePairPotential"] = BenchmarkGroup()
+SUITE["ParametricPairPotentialPairPotential"] = BenchmarkGroup()
 
 fname = joinpath(pkgdir(EmpiricalPotentials), "data", "TiAl-1024.xyz")
 data = ExtXYZ.load(fname) |> FastSystem
 
 lj = LennardJones(-1.0u"meV", 3.1u"Å",  13, 13, 6.0u"Å")
+ljp = LennardJones(-1.0u"meV", 3.1u"Å",  13, 13, 6.0u"Å"; parametric=true)
 
 nlist = PairList(data, cutoff_radius(lj))
 
@@ -31,3 +33,24 @@ SUITE["SimplePairPotential"]["energy"]["nlist given"] = @benchmarkable AtomsCalc
 SUITE["SimplePairPotential"]["energy_forces"]["no nlist given"] = @benchmarkable AtomsCalculators.energy_forces($data, $lj)
 SUITE["SimplePairPotential"]["energy_forces"]["nlist given"] = @benchmarkable AtomsCalculators.energy_forces($data, $lj; nlist=$nlist)
 
+
+SUITE["ParametricPairPotential"]["energy"] = BenchmarkGroup()
+SUITE["ParametricPairPotential"]["energy_forces"] = BenchmarkGroup()
+
+SUITE["ParametricPairPotential"]["energy"]["no nlist given"] = @benchmarkable AtomsCalculators.potential_energy($data, $ljp)
+SUITE["ParametricPairPotential"]["energy"]["nlist given"] = @benchmarkable AtomsCalculators.potential_energy($data, $ljp; nlist=$nlist)
+
+SUITE["ParametricPairPotential"]["energy_forces"]["no nlist given"] = @benchmarkable AtomsCalculators.energy_forces($data, $ljp)
+SUITE["ParametricPairPotential"]["energy_forces"]["nlist given"] = @benchmarkable AtomsCalculators.energy_forces($data, $ljp; nlist=$nlist)
+
+
+SUITE["ParametricPairPotential"]["parameter estimation"] = BenchmarkGroup()
+
+SUITE["ParametricPairPotential"]["parameter estimation"]["energy no nlist given"] = @benchmarkable AtomsCalculators.potential_energy($data, $ljp, $ljp.parameters)
+SUITE["ParametricPairPotential"]["parameter estimation"]["energy nlist given"] = @benchmarkable AtomsCalculators.potential_energy($data, $ljp, $ljp.parameters; nlist=$nlist)
+
+SUITE["ParametricPairPotential"]["parameter estimation"]["forces no nlist given"] = @benchmarkable AtomsCalculators.forces($data, $ljp, $ljp.parameters)
+SUITE["ParametricPairPotential"]["parameter estimation"]["forces nlist given"] = @benchmarkable AtomsCalculators.forces($data, $ljp, $ljp.parameters; nlist=$nlist)
+
+SUITE["ParametricPairPotential"]["parameter estimation"]["virial no nlist given"] = @benchmarkable AtomsCalculators.virial($data, $ljp, $ljp.parameters)
+SUITE["ParametricPairPotential"]["parameter estimation"]["virial nlist given"] = @benchmarkable AtomsCalculators.virial($data, $ljp, $ljp.parameters; nlist=$nlist)
