c6b27ef940bab16aefe93d651d2aaaae5c364c62
Teemu Järvinen <teemu.j.jarvinen@gmail.com>
2024-01-18 18:57:30 -0800

    add benchmarks for sw

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -61,3 +61,29 @@ SUITE["ParametricPairPotential"]["parameter estimation"]["forces nlist given"] =
 
 SUITE["ParametricPairPotential"]["parameter estimation"]["virial no nlist given"] = @benchmarkable AtomsCalculators.virial($data, $ljp, $ljp.parameters)
 SUITE["ParametricPairPotential"]["parameter estimation"]["virial nlist given"] = @benchmarkable AtomsCalculators.virial($data, $ljp, $ljp.parameters; nlist=$nlist)
+
+
+
+## Test StillingerWeber
+
+sw = StillingerWeber(; atom_number=13)
+
+fname = joinpath(pkgdir(EmpiricalPotentials), "data", "TiAl-1024.xyz")
+data = ExtXYZ.load(fname) |> FastSystem
+
+nlist = PairList(data, cutoff_radius(sw))
+
+SUITE["StillingerWeber"] = BenchmarkGroup()
+
+SUITE["StillingerWeber"]["energy"] = BenchmarkGroup()
+SUITE["StillingerWeber"]["energy_forces"] = BenchmarkGroup()
+SUITE["StillingerWeber"]["virial"] = BenchmarkGroup()
+
+SUITE["StillingerWeber"]["energy"]["no nlist given"] = @benchmarkable AtomsCalculators.potential_energy($data, $sw)
+SUITE["StillingerWeber"]["energy"]["nlist given"] = @benchmarkable AtomsCalculators.potential_energy($data, $sw; nlist=$nlist)
+
+SUITE["StillingerWeber"]["energy_forces"]["no nlist given"] = @benchmarkable AtomsCalculators.energy_forces($data, $sw)
+SUITE["StillingerWeber"]["energy_forces"]["nlist given"] = @benchmarkable AtomsCalculators.energy_forces($data, $sw; nlist=$nlist)
+
+SUITE["StillingerWeber"]["virial"]["no nlist given"] = @benchmarkable AtomsCalculators.virial($data, $sw)
+SUITE["StillingerWeber"]["virial"]["nlist given"] = @benchmarkable AtomsCalculators.virial($data, $sw; nlist=$nlist)
