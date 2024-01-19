1d462102ae7a4aaf61e27704a0350401cda419e5
Teemu Järvinen <teemu.j.jarvinen@gmail.com>
2024-01-18 18:47:34 -0800

    SW working correctly

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,4 +4,8 @@ using Test
 @testset "EmpiricalPotentials.jl" begin
     # Write your tests here.
     include("test_pairpotentials.jl")
+
+    @testset "StillingerWeber" begin
+        include("test_sw.jl")
+    end
 end
