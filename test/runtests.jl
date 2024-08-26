d4c00816477bbfe5257f4fc6bebfde6cbe5d6629
JuliaMolSim <christohortner@gmail.com>
2024-08-26 15:04:43 -0700

    cleanup zbl

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,5 +4,6 @@ using Test
 @testset "EmpiricalPotentials.jl" begin
     @testset "Lennard Jones" begin include("test_lennardjones.jl") end 
     @testset "Morse" begin include("test_morse.jl") end
+    @testset "ZBL" begin include("test_zbl.jl") end
     @testset "StillingerWeber" begin include("test_sw.jl") end
 end
--- test/test_zbl.jl
+++ test/test_zbl.jl
@@ -14,7 +14,7 @@ ACT = AtomsCalculatorsUtilities.Testing
 
 ##
 
-# generate the LJ potential 
+# generate the zbl potential 
 rcut = 7.0u"Å"
 V = ZBL(rcut)
 
@@ -31,15 +31,15 @@ V = ZBL(rcut)
 
 @info("ZBL Finite difference calculator test")
 
-function rand_struct(nrep) 
+function rand_struct_zbl(nrep) 
    sys = rattle!(bulk(:Al, cubic=true) * nrep, 0.1u"Å")
-   return randz!(sys, [ :Al => 0.5, :Cu => 0.5 ])
+   return randz!(sys, [ :Al => 0.3, :Cu => 0.3, :Si => 0.2, :Ge => 0.2 ])
 end 
 
-sys = rand_struct(2)
+sys = rand_struct_zbl(2)
 @test potential_energy(sys, V) isa Unitful.Energy
 @test forces(sys, V) isa Vector{<: SVector{3, <: Unitful.Force}}
 
-for sys in [ rand_struct(1), rand_struct(2), rand_struct(2) ]
+for sys in [ rand_struct_zbl(1), rand_struct_zbl(2), rand_struct_zbl(2) ]
    @test all( ACT.fdtest(sys, V; rattle = 0.01u"Å", verbose=false) )
 end
